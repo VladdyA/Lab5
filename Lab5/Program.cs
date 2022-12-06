0d903350d2dfee807ed251a7ab5d0e29a55d804e
User <User@DESKTOP-RPSJ6FD>
2022-12-06 18:02:41 +0200

    Add project files.

---
+++ Lab5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(25);
+
+            Console.WriteLine("Площа кола:" + circle1.Area());
+            Console.WriteLine("Площа кола:" + circle2.Area());
+            Console.WriteLine("Периметр кола:" + circle1.Perimeter());
+            Console.WriteLine("Периметр кола:" + circle2.Perimeter());
+
+            Square square1 = new Square(5);
+            Square square2 = new Square(25);
+
+            Console.WriteLine("Площа квадрату:" + square1.Area());
+            Console.WriteLine("Площа квадрату:" + square2.Area());
+            Console.WriteLine("Периметр квадрату:" + square1.Perimeter());
+            Console.WriteLine("Периметр квадрату:" + square2.Perimeter());
+
+            if (square1.Perimeter() > square2.Perimeter())
+            {
+                Console.WriteLine("Більшим периметром квадрату є " + square1.Area());
+            }
+            else
+            {
+                Console.WriteLine("Більшим  периметром квадрату є " + square2.Area());
+            }
+
+
+            if (circle1.Perimeter() > circle2.Perimeter())
+            {
+                Console.WriteLine("Більшим периметром кола є " + circle1.Area());
+            }
+            else
+            {
+                Console.WriteLine("Більшим периметром кола є " + circle2.Area());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
